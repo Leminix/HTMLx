1386967a8971ac9ea1409875bf92c3ac43dd1e30
filip <f.jaros05@seznam.cz>
2024-11-24 15:08:23 +0100

    handling variables

--- components/decisions.js
+++ components/decisions.js
@@ -22,6 +22,16 @@ const create = require('./create')
 //// this method decisions and sent data from htmlx file to others methods ////
 ///////////////////////////////////////////////////////////////////////////////
 
+    /////////////////////////////////////////////////////////////////////////////
+    //// this variable includes contents which will be write to a html file ////
+    ///////////////////////////////////////////////////////////////////////////
+
+    let content = ''
+
+    let helparray
+
+
+
 ///////////////////////////////////
 //// method expects file name ////
 /////////////////////////////////
@@ -41,18 +51,56 @@ module.exports = function decisions(file){
 
 
 
-    /////////////////////////////////////////////////////////////////////////////
-    //// this variable includes contents which will be write to a html file ////
-    ///////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////
+    //// array which contains objects of varibles ////
+    /////////////////////////////////////////////////
 
-    let content
+    let vars = []
+
+    ///////////////////////////////
+    //// reading line by line ////
+    /////////////////////////////
 
     obj.on('line', (line) => {
        // console.log(line)
-        if(line.includes('<') && line.includes('>')){
-            content = `${content} ${line}`
-            create(file, content)
+
+        if(line.includes('<') && line.includes('>') && !(line.includes('_'))){
+            //content = `${content} ${line}`
+            //content = content.concat(' ', line)
+            content += line
+        }
+        else if(line.includes('$') && line.includes('=')){
+            helparray = line.split('=')
+
+            //////////////////////////////////////////////////////////////
+            //// into array push object which has var name and value //// 
+            ////////////////////////////////////////////////////////////
+
+            vars.push({
+                name: helparray[0],
+                value: helparray[1].trim()
+            })
+        }
+        else if(line.includes('<') && line.includes('_')){
+            let varname = line.split('_')
+            content += line
+            
+            /////////////////////////////////////////////////
+            //// find a varible in array and save value ////
+            ///////////////////////////////////////////////
+           
+            vars.forEach(varObj => {
+                if (varObj.name === varname[1]) {
+                    content = content.replace(`_${varObj.name}_`, varObj.value)
+                }
+            })
+        }
+        else{
+            content = 'none'
         }
 
+    })
+    obj.on('close', () => {
+        create(file, content)
     })
 }
